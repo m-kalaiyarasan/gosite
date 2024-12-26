3ffb9f99cdcd14069861bba263cba268a99017bc
Kalaiyarasan Manoharan <160246890+m-kalaiyarasan@users.noreply.github.com>
2024-12-26 23:27:46 +0530

    Update load.php

--- htdocs/libs/load.php
+++ htdocs/libs/load.php
@@ -10,10 +10,7 @@ function load($name){
     include "_templates/$name.php";
 }
 Session::start();
-<<<<<<< HEAD
-=======
 
->>>>>>> 9950612fd860370fdf654e0285ac8b30e66e7baf
 global $__site_config;
 $__site_config_path = __DIR__.'/../../project/gosite.json';
 $__site_config = file_get_contents($__site_config_path);
