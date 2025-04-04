934ace553041cfdb6f192c6f57b74cbfd47c7599
kalaiyarasan.offl.2004@gmail.com <kalaiyarasan.offl.2004@gmail.com>
2025-04-04 13:54:07 +0000

    Start for wordpress

--- htdocs/test.php
+++ htdocs/test.php
@@ -1,5 +1,21 @@
-<div class="container mt-5">
+<!-- <div class="container mt-5">
         <span data-bs-toggle="tooltip" data-bs-placement="top" title="Caution: Read instructions carefully!">
             ℹ️
         </span>
-    </div>
+    </div> -->
+
+
+    <?php
+
+include 'libs/load.php';
+
+$wp = new Wordpress();
+$user_id = rand(100,200);
+
+$port = $wp->findAvailablePort();
+$create = $wp->setupWordPress($user_id,$port);
+
+echo"<pre>";
+print_r($create);
+echo"<pre>";
+print($port);
